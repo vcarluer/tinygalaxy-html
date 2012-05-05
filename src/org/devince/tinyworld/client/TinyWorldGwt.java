88a694f7368af67dcb18d5230df855e895762e2e
Vincent Carluer <vcarluer@gmail.com>
2012-05-05 02:43:32 +0200

    new version + manifest + media

--- src/org/devince/tinyworld/client/TinyWorldGwt.java
+++ src/org/devince/tinyworld/client/TinyWorldGwt.java
@@ -5,12 +5,14 @@ import org.devince.tinyworld.TinyWorld;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class TinyWorldGwt extends GwtApplication {
 
 	public GwtApplicationConfiguration getConfig() {
-		GwtApplicationConfiguration configuration = new GwtApplicationConfiguration(TinyWorld.WIDTH, TinyWorld.HEIGHT);
+		GwtApplicationConfiguration configuration = new GwtApplicationConfiguration(TinyWorld.WIDTH, TinyWorld.HEIGHT);		
 		configuration.useDebugGL = false;
+		configuration.rootPanel = RootPanel.get("container");		
 		return configuration;
 	}
 
